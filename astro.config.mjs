feb47d071c5977685194db061bda505568ec1b6b
guilty <sunymon@protonmail.com>
2024-04-10 00:54:40 -0400

    fix: output: hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://forgotten-shore.netlify.app",
-    integrations: [
-        sitemap(),
-        
-    ],
-    vite: {
+	site: "https://forgotten-shore.netlify.app",
+	integrations: [sitemap()],
+	vite: {
 		ssr: {
 			noExternal: ["smartypants"],
 		},
 	},
-    output: "hybrid",
 });
