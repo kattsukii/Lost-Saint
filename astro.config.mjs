fb3e1939fd3fa8450bf434acde5f1ab94e5d8bf3
guilty <sunymon@protonmail.com>
2024-04-10 00:50:46 -0400

    site: almost done with personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://forgotten-shore.netlify.app",
+    integrations: [
+        sitemap(),
+        
+    ],
+    vite: {
+		ssr: {
+			noExternal: ["smartypants"],
+		},
+	},
+    output: "hybrid",
+});
---
+++ src/data/site-info.ts
@@ -0,0 +1,61 @@
+export type SocialLink = {
+	platform: string;
+	href: string;
+	me?: string;
+	text: string;
+	icon: string;
+	footerOnly?: boolean;
+};
+
+export type SiteInfo = {
+	name: string;
+	title: string;
+	description: string;
+	image: {
+		src: string;
+		alt: string;
+	};
+	socialLinks: SocialLink[];
+};
+
+const siteInfo: SiteInfo = {
+	name: "Lost Saint",
+	title: "Personal website of David aka lost saint.",
+	description: "Personal website of David aka lost saint.",
+	image: {
+		src: "/og/social.jpg",
+		alt: "Personal website of David aka lost saint.",
+	},
+	socialLinks: [
+		{
+			platform: "github",
+			href: "https://github.com/withastro/astro",
+			me: "https://github.com/withastro/",
+			text: "Go to Astro's GitHub repo",
+			icon: "social/github",
+		},
+		{
+			platform: "discord",
+			href: "/chat",
+			text: "Join the Astro community on Discord",
+			icon: "social/discord",
+		},
+		{
+			platform: "twitter",
+			href: "https://twitter.com/astrodotbuild",
+			me: "https://twitter.com/astrodotbuild",
+			text: "Follow Astro on Twitter",
+			icon: "social/twitter",
+		},
+		{
+			platform: "mastodon",
+			href: "https://m.webtoo.ls/@astro",
+			me: "https://m.webtoo.ls/@astro",
+			text: "Follow Astro on Mastodon",
+			footerOnly: true,
+			icon: "social/mastodon",
+		},
+	],
+};
+
+export default siteInfo;
