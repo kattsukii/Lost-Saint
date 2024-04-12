b0f9cc70a56e46eb5e44f6faa45596c4397f9477
guilty <sunymon@protonmail.com>
2024-04-12 07:52:25 -0400

    chore: add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://forgotten-shore.netlify.app",
-	integrations: [sitemap()],
-	vite: {
-		ssr: {
-			noExternal: ["smartypants"],
-		},
-	},
+  site: "https://forgotten-shore.netlify.app",
+  integrations: [sitemap(), svelte()],
+  vite: {
+    ssr: {
+      noExternal: ["smartypants"]
+    }
+  }
 });
