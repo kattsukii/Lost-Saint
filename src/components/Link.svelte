301e90d0b8b20089e5e814c86e873d9410d12271
guilty <sunymon@protonmail.com>
2024-04-12 08:13:58 -0400

    site: add prject feed to home page & just need to style website

---
+++ src/components/Link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+export let href: string;
+</script>
+  
+  <a
+    {href}
+    target="_blank"
+    {...$$restProps}
+    class=""
+  >
+    <slot />
+  </a>
+  
+  <style>
+    /* Define your styles here */
+  </style>
+  
